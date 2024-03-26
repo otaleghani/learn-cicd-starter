2c75a5af87db22157993dcbf25b7e5ad85cd1815
Oliviero Taleghani <o.taleghani@talesign.com>
2024-03-26 18:52:21 +0100

    checks in place

--- json.go
+++ json.go
@@ -27,5 +27,9 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
+	_, err2 := w.Write(dat)
+	if err2 != nil {
+		w.WriteHeader(500)
+		return
+	}
 }
